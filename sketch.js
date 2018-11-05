59f53c04aef9b0299f16ab6957661ff4029ad6ac
Betu <bkay210@gmail.com>
2018-11-04 21:42:26 -0500

    first commit

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function make2DArray(cols, rows) {
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+}
+
+function Cell() {
+  this.bee = true;
+  this.revealed = true;
+}
+
+var grid;
+var cols = 20;
+var rows = 20;
+
+function setup() {
+  createCanvas(200, 200);
+  grid = make2DArray(cols, rows);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Cell();
+    }
+  }
+}
+
+function draw() {
+  background(0);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
+  }
+}
