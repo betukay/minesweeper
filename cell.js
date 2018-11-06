ecc6d26325bf2c9bb7f3a0cad0ccea3a1cff5974
Betu <bkay210@gmail.com>
2018-11-06 00:03:24 -0500

    add grid

---
+++ cell.js
@@ -0,0 +1,26 @@
+function Cell(x, y) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  if (random(1) < 0.5) {
+    this.bee = true;
+  } else {
+    this.bee = false;
+  }
+  this.revealed = false;
+}
+
+Cell.prototype.show = function() {
+  stroke(0);
+  noFill();
+  rect(this.x, this.y, this.w, this.w);
+  if (this.revealed) {
+    if (this.bee){
+      ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5 , this.w * 0.5);
+    }
+  }
+}
+
+Cell.prototype.contains = function (x, y) {
+  return (x > this. x && x < this.x + this.w && y > this.y && this.y < this.y + this.w);
+}
--- sketch.js
+++ sketch.js
@@ -3,29 +3,38 @@ function make2DArray(cols, rows) {
   for (var i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
   }
-}
-
-function Cell() {
-  this.bee = true;
-  this.revealed = true;
+  return arr;
 }
 
 var grid;
-var cols = 20;
-var rows = 20;
+var cols;
+var rows;
+var w = 20;
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(201, 201);
+  cols = floor(width / w);
+  rows = floor(height / w);
   grid = make2DArray(cols, rows);
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
-      grid[i][j] = new Cell();
+      grid[i][j] = new Cell(i * w, j * w, w);
+    }
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      if (grid[i][j].contains(mouseX, mouseY)) {
+        
+      }
     }
   }
 }
 
 function draw() {
-  background(0);
+  background(255);
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
       grid[i][j].show();
